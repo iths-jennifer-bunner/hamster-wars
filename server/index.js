f646582c4fe2066f86df52b04c9a8f688eb60544
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-06-10 15:54:59 +0200

    changed backend routes names.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.json())
 app.use(express.static('public'))
+app.use(express.static(__dirname + '/../build'))
 
 // let init = require('./initdb.js')
 
@@ -13,15 +16,15 @@ const gamesRoute = require('./routes/games')
 const statsRoute = require('./routes/stats')
 const assetsRoute = require('./routes/assets')
 
-app.use('/hamsters', hamstersRoute)
-app.use('/charts', chartsRoute)
-app.use('/games', gamesRoute)
-app.use('/stats', statsRoute)
-app.use('/assets', assetsRoute)
+app.use('/api/hamsters', hamstersRoute)
+app.use('/api/charts', chartsRoute)
+app.use('/api/games', gamesRoute)
+app.use('/api/stats', statsRoute)
+app.use('/api/assets', assetsRoute)
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up and running! ');
     
 })
