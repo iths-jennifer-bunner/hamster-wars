e4b435f40c6d0cfd80668841ccd8f8a85391fcc8
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-06-11 13:26:28 +0200

    test

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -1,22 +1,22 @@
 import React, {useState} from 'react';
 
 const Battle = () => {
-    const [hamsters, setHamsters] = useState(null);
-    const handleClick = async () => {
-        let array = await getHamsters();
-        console.log('Got hamsters from api:', array);
-        setHamsters(array);
-    }
-    // const [click1, setClick1] =useState(false)
-    // const [click2, setClick2] =useState(false)
+    // const [hamsters, setHamsters] = useState(null);
+    // const handleClick = async () => {
+    //     let array = await getHamsters();
+    //     console.log('Got hamsters from api:', array);
+    //     setHamsters(array);
+    //}
+    const [click1, setClick1] =useState(false)
+    const [click2, setClick2] =useState(false)
 
     return(
         <div>
             <p>Click the image that has the most fab hamster</p>
-            <button onClick={handleClick}>New contestants</button>
-            {/* <div onClick={() => setClick1(true)}>Pic 1{click1=== true ? ':I am the winner' : ''}</div>
-            <div onClick={() => setClick2(true)}>Pic 2{click2=== true ? ':I am the winner' : ''}</div> */}
-            <div>
+            {/* <button onClick={handleClick}>New contestants</button> */}
+            <div onClick={() => setClick1(true)}>Pic 1{click1=== true ? ':I am the winner' : ''}</div>
+            <div onClick={() => setClick2(true)}>Pic 2{click2=== true ? ':I am the winner' : ''}</div>
+            {/* <div>
             { hamsters
                         ? hamsters.map(hamster => (
                             <div key={hamster.id}>
@@ -24,24 +24,24 @@ const Battle = () => {
                             </div>
                         ))
                         : null }
-            </div>
+            </div> */}
         </div>
     )
 }
 
-async function getHamsters() {
-    let baseUrl = '/api';
-    try {
-        const response = await fetch(baseUrl + '/random');
-        console.log('response',response);
+// async function getHamsters() {
+//     let baseUrl = '/api';
+//     try {
+//         const response = await fetch(baseUrl + '/random');
+//         console.log('response',response);
         
-        const hamsterArray = await response.json();
-        return hamsterArray;
-    } catch (e) {
-        console.log('Fetch failed because', e);
-        return null;
-    }
-}
+//         const hamsterArray = await response.json();
+//         return hamsterArray;
+//     } catch (e) {
+//         console.log('Fetch failed because', e);
+//         return null;
+//     }
+// }
 
 export default Battle;
 
