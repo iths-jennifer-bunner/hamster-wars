fb85255b1094eb9d27132fac3230053a88eddaf4
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-06-11 15:31:25 +0200

    test

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use('/api/assets', assetsRoute)
 
 
 app.listen(port, () => {
-    console.log('Server is up and running! ');
+    console.log('Server is up and running on: ', port);
     
 })
 
