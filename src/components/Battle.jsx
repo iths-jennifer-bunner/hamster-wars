2f87a1cd81a037e57985738c1bf21b22eb29d87d
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-06-14 00:07:06 +0200

    added image to random hamster

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -1,47 +1,63 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
 
 const Battle = () => {
     const [hamster1, setHamster1] = useState(null);
     const [hamster2, setHamster2] = useState(null);
+    const [click1, setClick1] =useState(false)
+    const [click2, setClick2] =useState(false)
+
+    
     const handleClick1 = async () => {
         let hamster = await getHamsters();
-        console.log('Got hamsters from api:', hamster1);
         setHamster1(hamster);
     }
     const handleClick2 = async () => {
         let hamster = await getHamsters();
-        console.log('Got hamsters from api:', hamster1);
         setHamster2(hamster);
     }
-    const [click1, setClick1] =useState(false)
-    const [click2, setClick2] =useState(false)
+    
     return(
         <div>
             <p>Click the image that has the most fab hamster</p>
             <button onClick={handleClick1}>Contestant 1</button>
-            <button onClick={handleClick2}>Contestant</button>
-            <div onClick={() => setClick1(true)}>{click1=== true ? 'I am the winner' : ''}
-                { hamster1 ? <div >Contestant 1: is {hamster1.name}, {hamster1.age} years young, and likes {hamster1.loves} and eats {hamster1.favFood}.</div> : null }
-            </div>
-            <div onClick={() => setClick2(true)}>{click2=== true ? 'I am the winner' : ''}
-                { hamster2 ? <div >Contestant 1: is {hamster2.name}, {hamster2.age} years young, and likes {hamster2.loves} and eats {hamster2.favFood}.</div> : null }
-            </div>
+            <button onClick={handleClick2}>Contestant 2</button>
+            
+            { hamster1 ? <div > <StyledImg src={"./hamsters/" + hamster1.imgName} alt="A hamster"  onClick={() => setClick1(true)}/>
+            Contestant 1: is {hamster1.name}, {hamster1.age} years young, and likes {hamster1.loves} and eats {hamster1.favFood}.</div> : null }
+
+            { hamster2 ? <div > <StyledImg src={"./hamsters/" + hamster2.imgName} alt="A hamster" onClick={() => setClick2(true)}/>
+            Contestant 2: is {hamster2.name}, {hamster2.age} years young, and likes {hamster2.loves} and eats {hamster2.favFood}.</div> : null }
+
+            <DeclareWinner>
+                {click1=== true ? hamster1.name+ " is the winner!!!" : ''}
+                {click2=== true ? hamster2.name+' is the winner!!!' : ''}
+            </DeclareWinner>
         </div>
     )
 }
+const StyledImg= styled.img`
+    width: 15em;
+    cursor: pointer;
+`
+const DeclareWinner= styled.div`
+    font-size: large;
+    color: hotpink;
+`
 
 async function getHamsters() {
     let baseUrl = '/api';
     try {
         const response = await fetch(baseUrl + '/hamsters/random');
-        const hamster = await response.json();
-        console.log('response',response); 
+        const hamster = await response.json(); 
         return hamster;
     } catch (e) {
         console.log('Fetch failed because', e);
         return null;
     }
 }
+
+
 
 export default Battle;
 
